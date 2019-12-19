106c9ba4be91a5b95a8c5b1d3ff7f8b3ad820b7a
ov1618 <58977911+ov1618@users.noreply.github.com>
2019-12-19 22:46:20 +0530

    Add files via upload

---
+++ day6.py
@@ -0,0 +1,72 @@
+#using while
+'''
+started=0
+stopped=1
+while True:
+    a=input('ent a str')
+    if a=='start':
+        if started==1:
+            print('car already strtd')
+        else:
+            print('car start')
+            started=1
+    elif a=='stop':
+        if stopped==0:
+            print('car already stopped')
+        else:
+            print('car stop')
+            stopped=0
+    elif a=='exit':
+        break
+    elif a=='help':
+        print(start.....strt a car
+stop.....stop the car)
+    else:
+        print('i dont understand')
+    
+'''
+#convert kg to pound and vice versa
+'''
+name=input('ent the name')
+weight=int(input('ent the weight'))
+a=input('kg or lbs')
+if a=='kg':
+    ans=weight*float(2.2046)
+    print('hi',name,'your weight','in pound is',ans)
+if a=='lbs':
+    ans=weight%float(2.2046)
+    print('hi',name,'your weight','in kg is',ans)
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+            
+        
