5db1308c12db7e6dcd45cd566eb7b89a4a6ad716
ov1618 <58977911+ov1618@users.noreply.github.com>
2019-12-31 18:40:39 +0530

    Add files via upload

---
+++ day11.py
@@ -0,0 +1,23 @@
+
+'''
+sn=5
+count=0
+for i in range(3):
+    n=int(input('ent:'))
+    count+=1
+    if n==sn:
+        print('chance:',count)
+        print('u won')
+        break
+    else:
+        print('try')
+        print('chance:',count)
+else:
+    print('game over')
+'''
+
+
+
+    
+    
+        
