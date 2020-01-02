77a86f96119230954b75802ab735eb0d4e3a9fd4
ov1618 <58977911+ov1618@users.noreply.github.com>
2020-01-02 19:21:46 +0530

    Add files via upload

---
+++ day12.py
@@ -0,0 +1,19 @@
+#sorting
+'''
+a=[4,2,9,6,3,5]
+for i in range(len(a)):
+    min_val=min(a[i:])
+    min_index=a.index(min_val)
+    a[i],a[min_index]=a[min_index],a[i]
+print(a)   
+'''   
+    
+    
+        
+        
+            
+        
+        
+        
+        
+    
