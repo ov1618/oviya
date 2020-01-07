27286c6db09bb1e97b3d9a3f9f44276b282270f1
ov1618 <58977911+ov1618@users.noreply.github.com>
2020-01-07 19:20:02 +0530

    Add files via upload

---
+++ day14.py
@@ -0,0 +1,20 @@
+'''
+a=3
+b=[[0 for i in range(a)] for j in range(a)]
+for i in range(3):
+    for j in range(3):
+        print(b[i][j],end=' ')
+    print()
+'''
+
+#star pyramid
+'''
+num=5
+i=1
+while i<=num:         
+    print('*'*i)
+    i+=1
+'''
+
+
+
